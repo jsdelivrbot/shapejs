3e0daa1b40567788e2c950ba74ed638ef3251860
teak1 <trainian@gmail.com>
2017-02-06 14:02:13 -0800

    Update shape.js

--- v0.00.1/shape.js
+++ v0.00.1/shape.js
@@ -2,8 +2,8 @@ var files = ["box","triangle","vector"];
 var version = "v0.00.1";
 for(var i = 0;i<files.length;i++){
   var fileName = files[i];
-  var src = "https://raw.githubusercontent.com/teak1/shapejs/master/v0.00.1/"+fileName+".js";
+  var src = "https://rawgit.com/teak1/shapejs/master/"+version+"/"+fileName+".js";
   var script = document.createElement("script");
-  script.setAttrabute("src",src);
-  document.head.appendChild(script));
+  script.setAttribute("src",src);
+  document.head.appendChild(script);
 }
