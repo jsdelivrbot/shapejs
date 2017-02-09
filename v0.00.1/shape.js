e32c36fab2080a9f2893f94aa9f61f9df28e110b
teak1 <trainian@gmail.com>
2017-02-09 14:20:26 -0800

    Update shape.js

--- v0.00.1/shape.js
+++ v0.00.1/shape.js
@@ -8,3 +8,12 @@ for(var i = 0;i<files.length;i++){
   document.head.appendChild(script);
   console.log("loading "+files[i]+" from "+src);
 }
+files = ["https://rawgit.com/jriecken/sat-js/master/SAT.js","https://rawgit.com/jriecken/sat-js/master/SAT.min.js"];
+for(var i = 0;i<files.length;i++){
+  var fileName = files[i];
+  var src = fileName;
+  var script = document.createElement("script");
+  script.setAttribute("src",src);
+  document.head.appendChild(script);
+  console.log("loading "+files[i]+" from "+src);
+}
