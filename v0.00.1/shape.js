80e2c83aa4554d89c078c189ecd8bddb14a272f2
teak1 <trainian@gmail.com>
2017-02-10 13:36:57 -0800

    Update shape.js

--- v0.00.1/shape.js
+++ v0.00.1/shape.js
@@ -8,7 +8,7 @@ for(var i = 0;i<files.length;i++){
   document.head.appendChild(script);
   console.log("loading "+files[i]+" from "+src);
 }
-files = ["https://rawgit.com/jriecken/sat-js/master/SAT.js","https://rawgit.com/jriecken/sat-js/master/SAT.min.js"];
+files = ["https://rawgit.com/jriecken/sat-js/master/SAT.min.js","https://raw.githubusercontent.com/processing-js/processing-js/v1.4.8/processing.min.js"];
 for(var i = 0;i<files.length;i++){
   var fileName = files[i];
   var src = fileName;
