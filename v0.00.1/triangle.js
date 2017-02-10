4ea714d28bc223c29e65021078a1b82fd69fe99b
teak1 <trainian@gmail.com>
2017-02-10 08:29:40 -0800

    Update triangle.js

--- v0.00.1/triangle.js
+++ v0.00.1/triangle.js
@@ -1,10 +1,15 @@
-function triangle(){
-  this.point1 = new vector(a,b);
-  this.point2 = new vector(c,d);
-  this.point3 = new vector(e,f);
+function triangle(x1,y1,x2,y2,x3,y3,tags){
+  this.tags = tags;
+  this.points = [new SAT.V(x1,y1),new SAT.V(x2,y2),new SAT.V(x3,y3)];
+  this.mask = new SAT.Polygon(new SAT.V(),this.points);
 }
-triangle.prototype=Object.assign(triangle.prototype,{
-  getPoints(){
-    return new Array(this.point1,this.point2,this.point3)
+triangle.prototype = Object.assign(box.prototype,{
+  collide(other){
+    var response = new SAT.Response();
+    var collided = SAT.testPolygonPolygon(this.mask,other.mask, response);
+    return {response:response,isColliding:collided}
+  },
+  pointInShape(point){
+    return SAT.pointInPolygon(p,this.mesh);
   }
-})
+});
