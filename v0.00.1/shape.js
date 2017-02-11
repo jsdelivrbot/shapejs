916c3d720ee5fdf6e9930058065cd5ff480da8fd
teak1 <trainian@gmail.com>
2017-02-10 20:36:12 -0800

    Update shape.js

--- v0.00.1/shape.js
+++ v0.00.1/shape.js
@@ -20,7 +20,7 @@ for(var i = 0;i<files.length;i++){
 files = ["p5.dom","p5","p5.sound"];
 for(var i = 0;i<files.length;i++){
   var fileName = files[i];
-  var src = "https://rawgit.com/teak1/shapejs/master/"+fileName+".js";
+  var src = "https://rawgit.com/teak1/shapejs/master/req/"+fileName+".js";
   var script = document.createElement("script");
   script.setAttribute("src",src);
   document.head.appendChild(script);
